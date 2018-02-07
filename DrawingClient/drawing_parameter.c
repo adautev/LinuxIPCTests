edebabab164105aafc8e17f5e6e05ec300417b3c
adautev <andrei@dautev.eu>
2018-02-07 15:24:58 +0200

    Checking out mutexes too.

--- DrawingClient/drawing_parameter.c
+++
@@ -1,8 +0,0 @@
-enum command_type {
-    MOVE,
-    PAINT
-};
-struct message_buffer {
-    enum command_type command;
-    
-};
--- POSIXThreading/main.c
+++ POSIXThreading/main.c
@@ -5,43 +5,67 @@
 #include <stdlib.h>
 #include "phylosophers.h"
 sem_t kick_off; //An initial blocker, as there are no guarantees phylosophers will not try eating "at the same time"
-sem_t device[PHYLOSOPHERS_COUNT];
+sem_t semaphore_device[PHYLOSOPHERS_COUNT];
+pthread_mutex_t mutex_device[PHYLOSOPHERS_COUNT];
 
-void *have_lunch(void *arguments) { //Should parse arguments at some point. :D
+void *semaphore_have_lunch(void *arguments) { //Should parse arguments at some point. :D
     //Sleep random time, so concurrency can be simulated.
     int r = rand() % 10; 
-    sleep(r);
+    
     int phylosopher_number = (int *)arguments; //That's the point    
-    printf("%*cPhylosopher %d thinks...\n", phylosopher_number*4, ' ', phylosopher_number);        
+    printf("%*cSemaphore phylosopher %d thinks...\n", phylosopher_number*4, ' ', phylosopher_number);            
     short right_device = (phylosopher_number+1) % PHYLOSOPHERS_COUNT;
     short left_device = phylosopher_number;
-    printf("%*cPhylosopher %d waiting for left device %d and right device %d\n", phylosopher_number*4, ' ', phylosopher_number, left_device, right_device);
-    sem_wait(&device[left_device]);        
-    sem_wait(&device[right_device]);
-    printf("%*cPhylosopher %d eats...\n", phylosopher_number*4, ' ', phylosopher_number);
-    sem_post(&device[phylosopher_number]);
-    sem_post(&device[(phylosopher_number+1) % PHYLOSOPHERS_COUNT]);        
+    sleep(r);
+    printf("%*cSemaphore phylosopher %d waiting for left device %d and right device %d\n", phylosopher_number*4, ' ', phylosopher_number, left_device, right_device);
+    sem_wait(&semaphore_device[left_device]);        
+    sem_wait(&semaphore_device[right_device]);
+    printf("%*cSemaphore phylosopher %d eats...\n", phylosopher_number*4, ' ', phylosopher_number);
+    r = rand() % 10; 
+    sleep(r);
+    sem_post(&semaphore_device[phylosopher_number]);
+    sem_post(&semaphore_device[(phylosopher_number+1) % PHYLOSOPHERS_COUNT]);        
     pthread_exit(0);
 }
-
+void *mutex_have_lunch(void *arguments) { //Should parse arguments at some point. :D
+    //Sleep random time, so concurrency can be simulated.
+    int r = rand() % 10;
+    int phylosopher_number = (int *)arguments; //That's the point    
+    printf("%*cMutex phylosopher %d thinks...\n", phylosopher_number*4, ' ', phylosopher_number);        
+    sleep(r);
+    short right_device = (phylosopher_number+1) % PHYLOSOPHERS_COUNT;
+    short left_device = phylosopher_number;
+    printf("%*cMutex phylosopher %d waiting for left device %d and right device %d\n", phylosopher_number*4, ' ', phylosopher_number, left_device, right_device);
+    pthread_mutex_lock(&mutex_device[left_device]);        
+    pthread_mutex_lock(&mutex_device[right_device]);
+    printf("%*cMutex phylosopher %d eats...\n", phylosopher_number*4, ' ', phylosopher_number);
+    r= rand() % 10;
+    sleep(r);
+    pthread_mutex_unlock(&mutex_device[phylosopher_number]);
+    pthread_mutex_unlock(&mutex_device[(phylosopher_number+1) % PHYLOSOPHERS_COUNT]);     
+    pthread_exit(0);
+}
 int main(int argc, char **argv)
 {    
-    pthread_t phylosophers[PHYLOSOPHERS_COUNT]; 
+    pthread_t mutex_phylosophers[PHYLOSOPHERS_COUNT]; 
+    pthread_t semaphore_phylosophers[PHYLOSOPHERS_COUNT]; 
     //Initialize semaphores with initial value 1 + the let's fo semaphore, so it can hold off the hungry phylosophers. (:
    
     for(int i=0;i<PHYLOSOPHERS_COUNT;i++) {
-        sem_init(&device[i], 0, 1);    
+        sem_init(&semaphore_device[i], 0, 1);    
     }
     for (int i = 0; i < PHYLOSOPHERS_COUNT; i++) {
-        pthread_create(&phylosophers[i], NULL, &have_lunch, i);        
+        pthread_create(&semaphore_phylosophers[i], NULL, &semaphore_have_lunch, (void*)i);
+        pthread_create(&mutex_phylosophers[i], NULL, &mutex_have_lunch, (void*)i);
     }
 	
     for (int i = 0; i < PHYLOSOPHERS_COUNT; i++) {        
-        pthread_join(phylosophers[i], NULL);
+        pthread_join(semaphore_phylosophers[i], NULL);
+        pthread_join(mutex_phylosophers[i], NULL);
     }
     for (int i = 0; i < PHYLOSOPHERS_COUNT; i++) {        
-        sem_destroy(&device[i]);
+        sem_destroy(&semaphore_device[i]);
     }
     
-        return 0;
+    return 0;
 }
